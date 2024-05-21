9cd93787d98c5e6292640430c22122bf6b69b3e4
Jcam2346 <49297368+Jcam2346@users.noreply.github.com>
2024-05-21 20:51:43 +1000

    refactor sea_level_predictor.py

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -12,16 +12,15 @@ def draw_plot():
         plt.scatter(x = df.Year, y = (df['CSIRO Adjusted Sea Level']), marker = 'o', c = 'blue')
 
     # Create first line of best fit
-        x_values = np.arange(df['Year'].min(), 2051, 1)
+        x_val = np.arange(df['Year'].min(), 2051, 1)
         sloper, intercept, r, p, std_err = linregress(x = df.Year, y = (df['CSIRO Adjusted Sea Level']))
-        y_values = sloper * x_values + intercept
-        plt.plot(x_values,(sloper * x_values + intercept), linestyle = '--', c = 'black')
+        plt.plot(x_val,(sloper * x_val + intercept), linestyle = '--', c = 'black')
 
     # Create second line of best fit
-        df_2000 = df.loc[df.Year >= 2000]
+        df_2000 = df.loc[df['Year'] >= 2000]
         slope2, intercept2, r2, p2, std_err2 = linregress(x = df_2000.Year, y = (df_2000['CSIRO Adjusted Sea Level']))
-        x_values_2000 = np.arange(2000, 2051, 1)
-        plt.plot(x_values_2000,(x_values_2000*slope2+intercept2), linestyle = ':', c='red')
+        x_val_2000 = np.arange(2000, 2051, 1)
+        plt.plot(x_val_2000,(x_val_2000*slope2+intercept2), linestyle = ':', c='red')
 
     # Add labels and title
         plt.xlabel('Year')
